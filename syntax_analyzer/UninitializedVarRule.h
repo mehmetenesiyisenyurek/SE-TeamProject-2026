eb5356ba3b601bcb53ba070b17fe431835e1c195
Haci Omer Oguz <omeroguz720@gmail.com>
2026-05-06 17:44:48 +0300

    Add UninitializedVarRule skeleton

---
+++ syntax_analyzer/UninitializedVarRule.h
@@ -0,0 +1,30 @@
+#ifndef SE_TEAMPROJECT_2026_UNINITIALIZEDVARRULE_H
+#define SE_TEAMPROJECT_2026_UNINITIALIZEDVARRULE_H
+
+#include <string>
+#include <vector>
+
+#include "IRule.h"
+#include "../infrastructure/Diagnostic.h"
+
+class ASTNode;
+
+/*
+ * UninitializedVarRule:
+ * Başlatılmadan kullanılan değişkenleri tespit eder.
+ * Kural ID: R002
+ */
+class UninitializedVarRule : public IRule {
+public:
+
+    // Kural ID'sini döndürür
+    std::string getId() const override;
+
+    // Kural adını döndürür
+    std::string getName() const override;
+
+    // AST üzerinde başlatılmamış değişken analizi yapar
+    std::vector<Diagnostic> check(const ASTNode& ast) override;
+};
+
+#endif
